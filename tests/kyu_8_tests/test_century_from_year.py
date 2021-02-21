43b63a02cf9fffacf6b68c4eb465032e51445fe4
Renat Shakirov <killmash@gmail.com>
2021-02-21 21:44:37 +0300

    Update test_century_from_year.py

--- tests/kyu_8_tests/test_century_from_year.py
+++ tests/kyu_8_tests/test_century_from_year.py
@@ -11,3 +11,12 @@ class ComputationTestCase(unittest.TestCase):
     
     def test_equals_3(self):
         self.assertEqual(century(1601), 17, 'Testing for year 1601')
+    
+    def test_equals_4(self):
+        self.assertEqual(century(2000), 20, 'Testing for year 2000')
+    
+    def test_equals_5(self):
+        self.assertEqual(century(356), 4, 'Testing for year 356')
+    
+    def test_equals_6(self):
+        self.assertEqual(century(89), 1, 'Testing for year 89')
