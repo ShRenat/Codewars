f1cb1bf18c52691463854c556adea0be8c778608
Renat Shakirov <killmash@gmail.com>
2021-02-22 11:37:18 +0300

    Update test_grasshopper_summation.py

--- tests/kyu_8_tests/test_grasshopper_summation.py
+++ tests/kyu_8_tests/test_grasshopper_summation.py
@@ -5,7 +5,7 @@ from katas.kyu_8.grasshopper_summation import summation
 
 class SummationTestCase(unittest.TestCase):
     def test_equals(self):
-        self.assertEqual(summation(1), 2) # what if wrong
+        self.assertEqual(summation(1), 1)
 
     def test_equals_2(self):
         self.assertEqual(summation(8), 36)
